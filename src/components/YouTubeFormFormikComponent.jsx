3fe0d9feb6b31c32d98ea32fea08b60b68e96d77
Edwin Roman <pazamoryperdon@gmail.com>
2021-01-17 15:25:45 -0500

    "Formik component"

---
+++ src/components/YouTubeFormFormikComponent.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import {Formik} from 'formik'
+
+const initialValues={
+    name:'Edwin',
+    email:'',
+    channel:''
+}
+
+const onSubmit = values => {
+    console.log('form data',values);
+
+}
+
+const validate = values => {
+    let errors = {}
+    if(!values.name){
+        errors.name='Required'
+    }
+    if(!values.email){
+        errors.email='Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Formato invalido'
+    }
+    if(!values.channel){
+        errors.channel='Required'
+    }
+
+    return errors
+}
+
+function YouTubeFormFormikComponent() {    
+
+    
+
+    console.log('Visited fields: ' , formik.touched);
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <form onSubmit={formik.handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor="name">Nombre</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        // onChange={formik.handleChange} 
+                        // onBlur={formik.handleBlur}
+                        // value={formik.values.name}
+                        //con formik.getFieldProps estamos incluyendo onChange, onBlur y value
+                        {...formik.getFieldProps('name')}
+                    />
+                    {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div>: null}
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="email">E-mail</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        // onChange={formik.handleChange} 
+                        // onBlur={formik.handleBlur}
+                        // value={formik.values.email}
+                        {...formik.getFieldProps('email')}
+                    />
+                    {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div>: null}
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="channel">Canal</label>
+                    <input 
+                        type="text" 
+                        id="channel" 
+                        name="channel" 
+                        // onChange={formik.handleChange} 
+                        // onBlur={formik.handleBlur}
+                        // value={formik.values.channel}
+                        {...formik.getFieldProps('channel')}
+                    />
+                    {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div>: null}
+                </div>
+
+
+                <button type="submit">Submit</button>
+            </form>
+            
+        </Formik>
+    )
+}
+
+export default YouTubeFormFormikComponent
